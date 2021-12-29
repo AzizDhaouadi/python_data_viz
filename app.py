1800e2fba7deac22cd319a619e8547a06b6dc8a2
Aziz Dhaouadi <dhaziz.works@gmail.com>
2021-12-28 19:44:35 -0500

    Created the contact form route and created the database class and function for data handling

--- app.py
+++ app.py
@@ -6,14 +6,62 @@ import dash_core_components as dcc
 import plotly.graph_objects as go
 import plotly.express as px
 from dash.dependencies import Input, Output
+
 import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email
+import email_validator
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+class contactForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email(granular_message=True)], name='Inputemail')
+    message= TextAreaField(label='Your Message')
+    submit = SubmitField(label="Send Message")
 
 server = flask.Flask(__name__)
+server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///requests.db'
 
-@server.route('/')
+#Initialize the database
+db = SQLAlchemy(server)
+
+#Create db model
+class Requests(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    date_received = db.Column(db.DateTime, default=datetime.utcnow)
+
+    #Create function to return messages when values are added
+    def __repr__(self):
+        return '<Name %r' % self.id
+
+
+server.secret_key = "g?@jY6@yN9GXxBGcJ45BfmbDrfX5Fk&didn#MbD8"
+
+
+
+@server.route('/', methods=["GET", "POST"])
 def home():
-    return render_template('home.html')
+    form = contactForm()
+    if form.validate_on_submit():
+        print(f"E-mail:{form.email.data}, message:{form.message.data}")
+        request = Requests(email=form.email.data, message=form.message.data)
+        try:
+            db.session.add(request)
+            db.session.commit()
+            form.email.data = ''
+            form.message.data = ''
+            return redirect(url_for('home'))
+        except:
+            for error in form.errors.itervalues():
+                flash(error[0])
+            return "There was an error. Please try again later."
+
+    return render_template('home.html',form=form)
 
 
 external_stylesheets = [
